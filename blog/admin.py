588e4606e12d25c8b17894e69e6d8bf0ef1ad027
william <yourwilliam@gmail.com>
2016-05-14 23:57:02 +0800

    update the blog model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from blog.models import Category, Entry
 
 # Register your models here.
+
+class EntryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('content',{'fields': ['title','slug','content','status']}),
+        ('head image', {'fields':['head_image','head_image_url']}),
+        ('time', {'fields': ['publication_date','start_publication','end_publication','creation_date','last_update'], 'classes': ['collapse']}),
+        ('catalog',{'fields':['categorys']}),
+    ]
+    list_display = ('title', 'slug', 'status', 'creation_date', 'last_update')
+
+admin.site.register(Category)
+admin.site.register(Entry, EntryAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,38 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField("title", max_length=255)
+    slug = models.SlugField("slug", unique=True, max_length=255, help_text="Used to build the catagory's URL.")
+    description = models.TextField("description", blank=True)
+    # the TreeForignKey need to install the mptt package and import it , after to import it .
+    # parent = TreeForignKey('self', relate_name='children', null=True, blank=True, on_delete=models.SET_NULL, verbose_name=_('parent catagory'))
+    
+    def __unicode__(self):
+        return self.title
+    
+class Entry(models.Model):
+    
+    STATUS_CHOICES = ((0, 'draft'), (1, 'hidden'), (2, 'publish'))
+    title = models.CharField("title", max_length=255)
+    slug = models.CharField("slug", max_length=255)
+    status = models.IntegerField("status", db_index=True, choices=STATUS_CHOICES, default=0)
+    publication_date = models.DateTimeField("publication date", db_index=True, default=timezone.now, help_text="Used to build the entry's URL.")
+    start_publication = models.DateTimeField("start publication", db_index=True, blank=True, null=True, help_text="Start date of publication.")
+    end_publication = models.DateTimeField("end publication", db_index=True, blank=True, null=True, help_text="End date of publication.")
+    creation_date = models.DateTimeField("creation date", default=timezone.now)
+    last_update = models.DateTimeField("last update", default=timezone.now)
+    #content
+    content = models.TextField("content", blank=True)
+    # after to convert the content to html or markdown modle. add some method
+    categorys = models.ManyToManyField(Category, blank=True, related_name="entries", verbose_name="categories");
+
+
+    head_image_url = models.URLField("head image url", max_length=255, null=True, blank=True)
+    head_image = models.ImageField(upload_to='photos/%Y/%m',null=True)
+    def __unicode__(self):
+        return self.title
